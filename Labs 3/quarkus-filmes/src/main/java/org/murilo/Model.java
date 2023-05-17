aa328a16704b132f38ad75fc92d7f7ef7ffd282c
MurilowJr <murilobrito.jr@gmail.com>
2023-05-17 17:48:16 -0300

    Continuação repositorio LABS3

---
+++ Labs 3/quarkus-filmes/src/main/java/org/murilo/Model.java
@@ -0,0 +1,24 @@
+package org.murilo;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Model {
+    private List<Filmes> filmes = new LinkedList<>();
+
+    public Model(){
+        addFilme(new Filmes("Shrek", "120", "A Historia de um Ogro"));
+        addFilme(new Filmes("Mario", "110", "O Encanador mais famoso da Nintendo"));
+    }
+
+    public void addFilme(Filmes filme){
+        filmes.add(filme);
+    }
+
+    public Filmes buscarNome(String nome) {
+        for(Filmes filme:filmes){
+            if(filme.getNome().equals(nome)) return filme;
+        }
+        return null;
+    }
+}
