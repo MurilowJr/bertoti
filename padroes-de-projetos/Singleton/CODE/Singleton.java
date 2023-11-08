e51c299658bc848a800c8570997a6de50fefb6c7
MurilowJr <murilobrito.jr@gmail.com>
2023-11-07 21:30:31 -0300

    adição do padrão singleton

---
+++ padroes-de-projetos/Singleton/CODE/Singleton.java
@@ -0,0 +1,23 @@
+package CODE;
+
+public final class Singleton {
+    private static Singleton instance;
+    public String value;
+
+    private Singleton(String value) {
+        //Esta parte emula inicialização lenta
+        try{
+            Thread.sleep(1000);
+        } catch(InterruptedException ex) {
+            ex.printStackTrace();
+        }
+        this.value = value;
+    }
+
+    public static Singleton getInstance(String value) {
+        if(instance == null) {
+            instance = new Singleton(value);
+        }
+        return instance;
+    }
+}
